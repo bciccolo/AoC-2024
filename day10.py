5e93fb7fee5282ee41b32680d2d3166bb5b2e54a
Brian Ciccolo <brian@passingfocus.net>
2024-12-10 17:46:38 -0500

    Day 10, part 2

--- day10.py
+++ day10.py
@@ -1,14 +1,11 @@
 FILE = 'day10.dat'
 
-grid = []
-complete_trails = 0
-
-def count_trailheads():
+def count_trailheads(score):
     # last = 0
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 0:
-                follow_trail(y, x, 1, set())
+                follow_trail(y, x, 1, score, set())
                 # print('Trailhead @ ' + str(y) + ',' + str(x) + ' has ' + str(complete_trails - last))
                 # last = complete_trails
 
@@ -24,33 +21,39 @@ def load_data():
     file.close()
 
 
-def follow_trail(y, x, search_value, destinations):
+def follow_trail(y, x, search_value, score, destinations):
     global complete_trails
 
     location = (y, x)
-    if search_value == 10 and location not in destinations:
+    if search_value == 10 and (not score or (score and location not in destinations)):
         complete_trails += 1
         destinations.add(location)
         return
 
     # Check up
     if y > 0 and grid[y - 1][x] == search_value:
-        follow_trail(y - 1, x, search_value + 1, destinations)
+        follow_trail(y - 1, x, search_value + 1, score, destinations)
 
     # Check left
     if x > 0 and grid[y][x - 1] == search_value:
-        follow_trail(y, x - 1, search_value + 1, destinations)
+        follow_trail(y, x - 1, search_value + 1, score, destinations)
 
     # Check right
     if x < len(grid[y]) - 1 and grid[y][x + 1] == search_value:
-        follow_trail(y, x + 1, search_value + 1, destinations)
+        follow_trail(y, x + 1, search_value + 1, score, destinations)
 
     # Check down
     if y < len(grid) - 1 and grid[y + 1][x] == search_value:
-        follow_trail(y + 1, x, search_value + 1, destinations)
+        follow_trail(y + 1, x, search_value + 1, score, destinations)
 
 
+grid = []
 load_data()
-count_trailheads()
 
+complete_trails = 0
+count_trailheads(True)
 print("Part 1: " + str(complete_trails))
+
+complete_trails = 0
+count_trailheads(False)
+print("Part 2: " + str(complete_trails))
