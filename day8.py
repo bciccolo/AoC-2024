86f527ffddf8f4ff4edab8260d950ef8547290e9
Brian Ciccolo <brian@passingfocus.net>
2024-12-10 12:06:42 -0500

    Day 8, part 2

--- day8.py
+++ day8.py
@@ -1,12 +1,11 @@
 from pprint import pprint
 
-FILE = 'day8.dat'
+FILE = 'day8-snippet.dat'
 X = 1
 Y = 0
 
 grid = []
 nodes = dict()
-antinodes = []
 
 def check_bounds(y, x):
     return y >= 0 and y < len(grid) and x >= 0 and x < len(grid[y])
@@ -18,7 +17,6 @@ def load_data():
     for line in lines:
         line = line.strip()
         grid.append(list(line))
-        antinodes.append(list('.' * len(line)))
 
     file.close()
 
@@ -35,7 +33,11 @@ def find_nodes():
                 locations.append([y, x])
 
 
-def map_antinodes(resonate):
+def count_antinodes(resonate):
+    antinodes = []
+    for row in grid:
+        antinodes.append(list('.' * len(row)))
+
     for locations in nodes.values():
         for i in range(len(locations) - 1):
             for j in range(i + 1, len(locations)):
@@ -45,27 +47,42 @@ def map_antinodes(resonate):
                 dy = node_1[Y] - node_2[Y]
 
                 # Antinode 1
-                x = node_1[X] + dx
-                y = node_1[Y] + dy
-                if check_bounds(y, x):
-                    antinodes[y][x] = 'X'
+                x = node_1[X]
+                y = node_1[Y]
+                if resonate:
+                    while check_bounds(y, x):
+                        antinodes[y][x] = 'X'
+                        x += dx
+                        y += dy
+                else:
+                    x += dx
+                    y += dy
+                    if check_bounds(y, x):
+                        antinodes[y][x] = 'X'
 
                 # Antinode 2
-                x = node_2[X] - dx
-                y = node_2[Y] - dy
-                if check_bounds(y, x):
-                    antinodes[y][x] = 'X'
+                x = node_2[X]
+                y = node_2[Y]
+                if resonate:
+                    while check_bounds(y, x):
+                        antinodes[y][x] = 'X'
+                        x -= dx
+                        y -= dy
+                else:
+                        x -= dx
+                        y -= dy
+                        if check_bounds(y, x):
+                            antinodes[y][x] = 'X'
+
+    count = 0
+    for row in antinodes:
+        count += row.count('X')
+
+    return count
 
 
 load_data()
-# pprint(grid)
 find_nodes()
-# pprint(nodes)
-map_antinodes(True) # Pass False for part 1, True for part 2
-# pprint(antinodes)
 
-count = 0
-for row in antinodes:
-    count += row.count('X')
-
-print('Answer: '  + str(count))
+print('Part 1: '  + str(count_antinodes(False)))
+print('Part 2: '  + str(count_antinodes(True)))
