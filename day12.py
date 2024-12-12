c30787047f10c0933e10e7b9d602fb660e5beba3
Brian Ciccolo <brian@passingfocus.net>
2024-12-12 05:29:45 -0500

    Day 12, part 1

--- day12.py
+++ day12.py
@@ -1,4 +1,77 @@
-FILE = 'day12-snippet.dat'
+import copy
+
+FILE = 'day12.dat'
+
+plants = set()
+plots = []
+
+def calculate_price():
+    total = 0
+
+    for plant in plants:
+
+        plots_copy = copy.deepcopy(plots)
+        region_index = 0
+        for y in range(len(plots_copy)):
+            for x in range(len(plots_copy[y])):
+                if plots_copy[y][x] == plant:
+                    map_region(plots_copy, plant, region_index, y, x)
+                    total += calculate_region_price(plots_copy, plant + str(region_index))
+                    region_index += 1
+
+    return total
+
+
+def calculate_region_price(map, plant):
+    area = 0
+    perimeter = 0
+
+    for y in range(len(map)):
+        for x in range(len(map[y])):
+            if map[y][x] == plant:
+                area += 1
+                perimeter += 4
+
+                # Check up
+                if y > 0 and map[y - 1][x] == plant:
+                    perimeter -= 1
+
+                # Check down
+                if y < len(map) - 1 and map[y + 1][x] == plant:
+                    perimeter -= 1
+
+                # Check left
+                if x > 0 and map[y][x - 1] == plant:
+                    perimeter -= 1
+
+                # Check right
+                if x < len(map[y]) - 1 and map[y][x + 1] == plant:
+                    perimeter -= 1
+
+    price = area * perimeter
+    print('A region of ' + plant + ' plants with price ' + str(area) + ' * ' + str(perimeter) + ' = ' + str(price))
+
+    return price
+
+def map_region(map, plant, index, y, x):
+    map[y][x] = plant + str(index)
+
+    # Check up
+    if y > 0 and map[y - 1][x] == plant:
+        map_region(map, plant, index, y - 1, x)
+
+    # Check down
+    if y < len(map) - 1 and map[y + 1][x] == plant:
+        map_region(map, plant, index, y + 1, x)
+
+    # Check left
+    if x > 0 and map[y][x - 1] == plant:
+        map_region(map, plant, index, y, x - 1)
+
+    # Check right
+    if x < len(map[y]) - 1 and map[y][x + 1] == plant:
+        map_region(map, plant, index, y, x + 1)
+
 
 def load_data():
     file = open(FILE, 'r')
@@ -7,10 +80,12 @@ def load_data():
     for line in lines:
         line = line.strip()
         row = list(line)
-        grid.append(row)
+        plants.update(row)
+        plots.append(row)
 
     file.close()
 
-grid = []
 load_data()
-print(grid)
+price = calculate_price()
+
+print("Part 1: " + str(price))
