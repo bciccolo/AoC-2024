35649c7a2e813f230440a841f70cb7e6ef72c0ec
Brian Ciccolo <brian@passingfocus.net>
2024-12-17 07:18:32 -0500

    Day 17, part 2 - working for example data only

--- day17.py
+++ day17.py
@@ -1,8 +1,14 @@
-# Day 17 snippet data
+# Day 17 snippet data A
 # register_a = 729
 # register_b = 0
 # register_c = 0
 # instructions = [0,1,5,4,3,0]
+
+# Day 17 snippet data B
+# register_a = 2024
+# register_b = 0
+# register_c = 0
+# instructions = [0,3,5,4,3,0]
 
 # Day 17 full data
 register_a = 44374556
@@ -22,45 +28,76 @@ def combo(operand):
     else:
         print('Invalid combo operand')
 
-output = []
+def run_program(replicate):
+    global register_a, register_b, register_c
 
-i = 0
-while i < len(instructions):
-    opcode = instructions[i]
-    operand = instructions[i + 1]
+    output = []
 
-    if opcode == 0:
-        numerator = register_a
-        denominator = pow(2, combo(operand))
-        register_a = int(numerator / denominator)
+    i = 0
+    while i < len(instructions):
+        opcode = instructions[i]
+        operand = instructions[i + 1]
 
-    elif opcode == 1:
-        register_b = register_b ^ operand
+        if opcode == 0:
+            numerator = register_a
+            denominator = pow(2, combo(operand))
+            register_a = int(numerator / denominator)
 
-    elif opcode == 2:
-        register_b = combo(operand) % 8
+        elif opcode == 1:
+            register_b = register_b ^ operand
 
-    elif opcode == 3: # Jump
-        if register_a != 0:
-            i = operand
-            continue
+        elif opcode == 2:
+            register_b = combo(operand) % 8
 
-    elif opcode == 4:
-        register_b = register_b ^ register_c
+        elif opcode == 3: # Jump
+            if register_a != 0:
+                i = operand
+                continue
 
-    elif opcode == 5:
-        output.append(combo(operand) % 8)
+        elif opcode == 4:
+            register_b = register_b ^ register_c
 
-    elif opcode == 6:
-        numerator = register_a
-        denominator = pow(2, combo(operand))
-        register_b = int(numerator / denominator)
+        elif opcode == 5:
+            output.append(combo(operand) % 8)
 
-    elif opcode == 7:
-        numerator = register_a
-        denominator = pow(2, combo(operand))
-        register_c = int(numerator / denominator)
+            if replicate:
+                if len(output) > len(instructions):
+                    return []
+                last_index = len(output) - 1
+                if output[last_index] != instructions[last_index]:
+                    return []
 
-    i += 2
+        elif opcode == 6:
+            numerator = register_a
+            denominator = pow(2, combo(operand))
+            register_b = int(numerator / denominator)
 
+        elif opcode == 7:
+            numerator = register_a
+            denominator = pow(2, combo(operand))
+            register_c = int(numerator / denominator)
+
+        i += 2
+
+    return output
+
+
+output = run_program(False)
 print('Part 1: ' + ','.join([str(x) for x in output]))
+
+min_value = 0
+while True:
+    register_a = min_value
+    register_b = 0
+    register_c = 0
+
+    output = run_program(True)
+    if len(output) == len(instructions):
+        break
+
+    if min_value % 1_000_000 == 0:
+        print(min_value)
+
+    min_value += 1
+
+print('Part 2: ' + str(min_value)) # 20_020_000_000 is too low based on puzzle feedback
